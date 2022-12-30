05957f33cfd3b01ec5c0b10f6691b77496038bd7
Klop233 <3337913379@qq.com>
2022-12-30 16:37:54 +0800

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,20 @@
 <template>
-  <button @click=getLoli>随机色图</button>
+  <div class="container">
+    <el-button type="success" @click="fetchData">Random words</el-button>
+  </div>
   <br>
-  <h1 id="content"></h1>
+  <p id="content" class="text">d</p>
+
 </template>
 
 <script>
 import axios from 'axios'
+import ElementPlus from "element-plus";
 
 export default {
   name: "index",
   methods: {
-    getLoli() {
+    fetchData() {
       axios.get('https://api.mcloc.cn/words').then(function(res) {
         console.log("Fetch data -> ", res)
         document.getElementById("content").innerHTML = res.data
@@ -19,3 +23,19 @@ export default {
   }
 }
 </script>
+
+<style>
+.button {
+  margin: 0 auto;
+}
+
+.text {
+  text-align: center;
+}
+
+.container {
+  display: flex;
+  justify-content: center;
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import ElementPlus from 'element-plus'
+import 'element-ui/lib/theme-chalk/index.css'
 import './assets/main.css'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(ElementPlus)
+    .mount('#app')
