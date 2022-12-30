ee750c0f9942b0f271ccc54c2f188f2dcee7cb40
Klop233 <3337913379@qq.com>
2022-12-30 15:55:26 +0800

    .

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <button @click=getLoli>随机色图</button>
+  <br>
+  <h1 id="content"></h1>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: "index",
+  methods: {
+    getLoli() {
+      axios.get('https://api.mcloc.cn/words').then(function(res) {
+        console.log("Fetch data -> ", res)
+        document.getElementById("content").innerHTML = res.data
+      })
+    }
+  }
+}
+</script>
