5e0d4184f933169b5a245ae47711d45cadf86aa4
Klop233 <3337913379@qq.com>
2022-12-30 16:50:01 +0800

    fix build problem

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
-import 'element-ui/lib/theme-chalk/index.css'
+import 'element-plus/dist/index.css'
 import './assets/main.css'
 
 createApp(App)
