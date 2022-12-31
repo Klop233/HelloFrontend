d0dc28ba8be7f6410e2723ad3d620ea2cfbd9675
Klop233 <3337913379@qq.com>
2022-12-31 11:36:30 +0800

    fix

--- src/components/index.vue
+++ src/components/index.vue
@@ -172,9 +172,11 @@ export default {
 
 <style>
 .center {
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
 }
 
 .box-card {
